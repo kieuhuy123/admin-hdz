a93680c2302a72f4677c4f9aef9965fe43cbfbf1
kieuhuy <huypk.codetot@gmail.com>
2023-11-03 01:00:34 +0700

    Add loading

--- src/app/(dashboard)/[storeId]/(routes)/colors/loading.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/colors/loading.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-// import { Loader } from "@/components/ui/loader";
+import { Loader } from '@/components/ui/loader'
 
 const Loading = () => {
   return (
     <div className='flex h-full w-full items-center justify-center'>
-      {/* <Loader /> */}
+      <Loader />
     </div>
   )
 }
