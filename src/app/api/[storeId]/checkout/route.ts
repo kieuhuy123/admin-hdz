e4efbe16dd7059d7801597d19ca3257c5ad2d27a
kieuhuy <huypk.codetot@gmail.com>
2023-11-03 16:42:32 +0700

    Create api order

---
+++ src/app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server'
+
+import prismadb from '@/lib/prismadb'
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+}
+
+export async function OPTIONS () {
+  return NextResponse.json({}, { headers: corsHeaders })
+}
+
+export async function POST (
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  const { productIds, address, phone } = await req.json()
+
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse('Product ids are required', { status: 400 })
+  }
+
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: true,
+      address,
+      phone,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId
+            }
+          }
+        }))
+      }
+    }
+  })
+
+  return NextResponse.json(
+    { url: `${process.env.FRONTEND_STORE_URL}/cart?success=1` },
+
+    {
+      headers: corsHeaders
+    }
+  )
+}
